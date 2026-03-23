724299e033e387eacbf2ff80a247f0bfc7ccf920
Assersaleh459 <assersaleh200@gmail.com>
2026-03-23 07:36:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vyra-v20';
+const CACHE = 'vyra-v21';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
